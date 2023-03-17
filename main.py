175fa3de19151c4935094301f7582d52bd97b3be
Pranoy Majumdar <kumarricky252@gmail.com>
2023-03-17 10:51:32 +0000

    done

--- main.py
+++ main.py
@@ -11,35 +11,10 @@ async def on_startup():
     pass
 
 
-@app.command(name="rolemenu", description="Rolemenu command")
-async def rolemenu(interaction): pass
+@app.command(name="alive", description="alive ?")
+async def alive(interaction: Interaction):
+    await interaction.respond("Yes im alive :), Thanks to Pranoy & Dismake Daddy.")
 
-
-@rolemenu.sub_command(name="create", description="Okiee?")
-async def rc(interaction): pass
-
-@rc.sub_command(name='something', description="Hmmm")
-async def rcs(interaction: Interaction):
-    await interaction.respond("Called.")
-
-@rolemenu.sub_command(name="delete", description="Delete okiee??", options=[
-    dismake.Option(name="cname", description="cname", required=True),
-    dismake.Option(name="buttons", description="buttons", required=True, choices=[
-        dismake.Choice(name='B1'),
-        dismake.Choice(name='B2'),
-        dismake.Choice(name='B3'),
-        dismake.Choice(name='B4'),
-    ]),
-])
-async def rd(interaction: Interaction):
-    await interaction.respond("delete")
-
-@app.command(
-    name="ban",
-    description="Ban a randi"
-)
-async def ban(interaction: Interaction):
-    await interaction.respond("ban")
 if __name__ == "__main__":
     app.run(app=f"main:app", reload=True)
 
