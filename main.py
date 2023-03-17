17719f33c39a0de51002563266f8084aafc47eb7
Pranoy Majumdar <kumarricky252@gmail.com>
2023-03-17 11:35:55 +0000

    Hmm

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import dismake, config
+from dismake.enums import OptionType
 from dismake.interaction import Interaction
 
 app = dismake.Bot(
     token=config.token, client_public_key=config.public_key, client_id=config.client_id
 )
+
 
 @app.on_event("startup")
 async def on_startup():
@@ -11,10 +13,43 @@ async def on_startup():
     pass
 
 
-@app.command(name="alive", description="alive ?")
+code = """
+@app.command(name="echo", description="Echo", options=[
+    dismake.Option(name='text', description="Say something.", required=True),
+    dismake.Option(name='show_code', description="If this option is choosed then i will show you the code of this command.", required=True, type=OptionType.BOOLEAN),
+])
 async def alive(interaction: Interaction):
-    await interaction.respond("Yes im alive :), Thanks to Pranoy & Dismake Daddy.")
+    if interaction.data.options[1].value == "False":
+        await interaction.respond(f"**You Said**: \n{interaction.data.options[0].value}")
+    else:
+        pass # Skipped for some pertional reason.
+
+"""
+
+
+@app.command(
+    name="echo",
+    description="Echo",
+    options=[
+        dismake.Option(name="text", description="Say something.", required=True),
+        dismake.Option(
+            name="show_code",
+            description="If this option is choosed then i will show you the code of this command.",
+            required=True,
+            type=OptionType.BOOLEAN,
+        ),
+    ],
+)
+async def alive(interaction: Interaction):
+    if interaction.data.options[1].value == "False":
+        await interaction.respond(
+            f"**You Said**: \n{interaction.data.options[0].value}"
+        )
+    else:
+        await interaction.respond(
+            f"**You Said**: \n{interaction.data.options[0].value}\n\n**Code**: \n```py\n{code}\n```"
+        )
+
 
 if __name__ == "__main__":
     app.run(app=f"main:app", reload=True)
-
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as file:
+    long_description = file.read()
+setup(
+    name='dismake',
+    version='0.0.3',
+    author="Pranoy Majumdar",
+    author_email="officialpranoy2@gmail.com",
+    description="None",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PranoyMajumdar/dismake",
+    project_urls={
+        "Homepage": "https://github.com/PranoyMajumdar/dismake"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.9",
+    license="MIT",
+    install_requires=[
+        'fastapi',
+        'pydantic',
+        'httpx',
+        'uvicorn',
+        'PyNaCl',
+        'rich',
+
+    ]
+)
