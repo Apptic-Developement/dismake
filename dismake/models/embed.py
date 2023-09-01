95ebb2ebbf3c0d101cc2e7254e234195044f42a7
Pranoy Majumdar <kumarricky252@gmail.com>
2023-09-01 04:38:15 +0000

    Adding embed field

--- dismake/models/embed.py
+++ dismake/models/embed.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Optional, Sequence, Tuple
+from typing import TYPE_CHECKING, Any, Dict, Optional, Sequence, Tuple, Type
 from dataclasses import dataclass, field
 
-from dismake.types.embed import EmbedAuthorData
+from dismake.types.embed import EmbedAuthorData, EmbedFieldData, EmbedFooterData
 
 if TYPE_CHECKING:
     from typing_extensions import Self
@@ -40,6 +40,34 @@ class EmbedFooter:
     text: Optional[str] = field(default=None, hash=True)
     icon_url: Optional[str] = field(default=None, hash=True)
     proxy_icon_url: Optional[str] = field(default=None, hash=True)
+
+    def to_dict(self) -> Optional[EmbedFooterData]:
+        if self.text is not None:
+            base: EmbedFooterData = {"text": self.text}
+            if self.icon_url is not None:
+                base["icon_url"] = self.icon_url
+
+            if self.proxy_icon_url is not None:
+                base["proxy_icon_url"] = self.proxy_icon_url
+
+            return base
+        return None
+
+
+@dataclass()
+class EmbedField:
+    name: Optional[str] = field(default=None, hash=True)
+    value: Optional[str] = field(default=None, hash=True)
+    inline: Optional[bool] = field(default=None, hash=True)
+
+    def to_dict(self) -> Optional[EmbedFieldData]:
+        if self.name is not None and self.value is not None:
+            base: EmbedFieldData = {"name": self.name, "value": self.value}
+            if self.inline is not None:
+                base["inline"] = self.inline
+
+            return base
+        return None
 
 
 class Embed:
@@ -78,30 +106,26 @@ class Embed:
         # Create a new instance of the class
         embed = cls.__new__(cls)
 
-        embed.type = data['type']
+        # Set the 'type' attribute from the data dictionary
+        embed.type = data["type"]
 
-        embed.title = data.get('title')
+        # Loop through specified fields and set them as attributes of the embed object
+        for field in ("title", "description", "url", "color"):
+            setattr(embed, field, data.get(field))
 
-        embed.description = data.get('description')
-
-        embed.url = data.get('url')
-
-        embed.color = data.get('color')
-
-        embed.timestamp = data.get('timestamp')  # TODO: Consider how to handle 'timestamp'
-        
+        # Set the 'timestamp' attribute from the data dictionary
+        embed.timestamp = data.get(
+            "timestamp"
+        )  # TODO: Consider how to handle 'timestamp'
 
         # Define a dictionary mapping attribute names to their corresponding classes
-        attrs = {
-            "author": EmbedAuthor,
-            "footer": EmbedFooter,
-        }
+        attrs: Dict[str, Type[Any]] = {"author": EmbedAuthor, "footer": EmbedFooter}
 
-        # Loop through all the attributes stored in the 'attrs' dictionary
+        # Loop through attributes defined in the 'attrs' dictionary
         for attr_name, attr_class in attrs.items():
             # Check if the attribute exists in the data dictionary
             if (attr_data := data.get(attr_name)) is not None:
-                # If the attribute name exists in the data payload, create an instance of the attribute class
+                # Create an instance of the attribute class using data
                 attr_instance = attr_class(**attr_data)
                 # Set the attribute in the new embed object with a prefixed name (e.g., self._author)
                 setattr(embed, "_" + attr_name, attr_instance)
@@ -115,7 +139,7 @@ class Embed:
         # Iterate through all the attributes defined in __slots__
         for attr in self.__slots__:
             # Check if the attribute name starts with an underscore ('_') indicating
-            # it's an embed attribute (eg., author, footer)
+            # it's an embed attribute (e.g., _author, _footer)
             if attr.startswith("_"):
                 try:
                     # Fetch the attribute
@@ -123,8 +147,10 @@ class Embed:
                 except AttributeError:
                     pass
                 else:
-                    # Convert the fetched attribute to a dictionary using its to_dict method and adding it to our base dict.
-                    base[attr[1:]] = fetched_attr.to_dict()
+                    # Convert the fetched attribute to a dictionary using its to_dict method
+                    # and add it to our base dictionary.
+                    # We use type: ignore here because mypy incorrectly flags this line due to dynamic key generation.
+                    base[attr[1:]] = fetched_attr.to_dict()  # type: ignore
 
         # Check and add 'title' if it exists
         if self.title is not None:
--- tests/test_embed.py
+++ tests/test_embed.py
@@ -2,11 +2,9 @@ from dismake import Embed
 from discord import Embed as DEmbed
 from pprint import pprint as print
 
-demb = DEmbed(title="Okk", description="Hmm").set_author(name="Okk")
+demb = DEmbed(title="Okk", description="Hmm").set_author(name="Okk").set_footer(text="Okk", icon_url="https://google.com/")
 
 embed = Embed(title="Test Title", description="Test Description")
 
-print(Embed.from_dict({'author': {'name': 'Okk'},
- 'description': 'Hmm',
- 'title': 'Okk',
- 'type': 'rich'}).to_dict())
+print(Embed.from_dict(demb.to_dict()).to_dict())
+
